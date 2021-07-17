957c82f428e400fc68ab1627ecf7bd2ab797510c
AUTUMN <krshlee22@naver.com>
2021-07-17 13:47:13 +0900

    first commit

---
+++ hello world.js
@@ -0,0 +1,17 @@
+var name = "kim"
+let name2 = "un"
+const name3 = "dong"
+// var, let = 변수를 만들고 자유롭게 수정이 가능, var는 구식
+// const = 변수를 만들고 수정이 불가능
+
+
+let number = 0; //정수
+let float = 180.4;   //실수
+let string = "Hello";   //문자열
+let bool = false;   // 참,거짓
+let bool2 = true;
+let tmp;
+
+console.log(`철수의 키는 ${float} 입니다`);
+
+console.log(typeof number);
